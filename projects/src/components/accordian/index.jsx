2f9d6439e078b9fa03094ddaf676d0f90e64bc6f
Abbosjon Karshiev <s284350@studenti.polito.it>
2025-09-11 17:26:53 +0200

    multiple toggle enabled , button added for allowing multiple toggle

--- projects/src/components/accordian/index.jsx
+++ projects/src/components/accordian/index.jsx
@@ -4,18 +4,31 @@ import data from './data';
 export default function Accordian(){
     const [accordianData, setAccordianData] = useState(data);
     const [selected, setSelected] = useState(null);
-    const clickHandler = (id) =>{
-     selected === id ? setSelected(null) : setSelected(id);}
-
-return(
+    const [selectedMult, setSelectedMult] = useState([]);
+    const [enableMult, setEnableMult] = useState(false);
+    const clickHandler = (id) =>{selected === id ? setSelected(null) : setSelected(id);}
+    const multipleClickHandler = (id) =>{
+      const toggledQuestions = [...selectedMult];
+      
+        const index = toggledQuestions.indexOf(id);
+        if(index > -1){
+      
+      setSelectedMult(toggledQuestions.filter((item)=> item !== id));
+        }else{
+        toggledQuestions.push(id);
+        setSelectedMult(toggledQuestions)
+    }}
+        return(
     <div className="accordian">
+        <button  onClick={()=> setEnableMult(!enableMult) } style={{borderColor:'white'}} >Enable Mult</button>
         {data.map((item) => (
             <>
-            <div className="accordian-question" id={item.id} onClick={()=>clickHandler(item.id)} >
+            <div className="accordian-question"  id={item.id} onClick={enableMult? ()=>multipleClickHandler(item.id): ()=>clickHandler(item.id)} >
                 {item.question}
             </div>
+            <span>+</span>
             <div className="accordian-answer" id={item.id}>
-              {selected===item.id ? item.answer : null}
+              { !enableMult ? (selected===item.id && (<div>{item.answer}</div>) ): (selectedMult.includes(item.id) ? item.answer: null)}
             </div>
             </>
         ))}
